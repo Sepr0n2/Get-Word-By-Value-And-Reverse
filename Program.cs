460ad509c38e3115bc9b88f2a434c65f47a9540e
Sepr0n2 <turpansaid@gmail.com>
2025-05-28 21:33:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = 0;
+        while (n <= 0)
+        {
+            System.Console.WriteLine("Kaç kelime gircen");
+            string inputn = Console.ReadLine();
+            if (int.TryParse(inputn, out n) && n > 0)
+            {
+                System.Console.WriteLine("Tamam");
+
+            }
+            else
+            {
+                System.Console.WriteLine("Pozitif gir");
+            }
+        }
+        string[] kelimedizisi = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            System.Console.WriteLine("{0}.kelimeyi gir", i + 1);
+            kelimedizisi[i] = Console.ReadLine();
+        }
+        Array.Reverse(kelimedizisi);
+        System.Console.WriteLine("EN SEVDİĞİN KELİMELER");
+        foreach (var kelime in kelimedizisi)
+        {
+            System.Console.WriteLine(kelime);
+        }
+
+
+
+
+
+    }
+}
